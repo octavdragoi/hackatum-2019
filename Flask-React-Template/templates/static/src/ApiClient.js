13d7eac9226d2000665a5ec35f6d8ec82dbecaab
octavdragoi <octavdragoi@alumni.harvard.edu>
2019-11-23 13:51:11 +0100

    changed python pages a bit

--- Flask-React-Template/templates/static/src/ApiClient.js
+++ Flask-React-Template/templates/static/src/ApiClient.js
@@ -1,7 +1,8 @@
 const https = require('https');
 
 export function test(callback) {
-  https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  //https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  https.get('localhost:5000/api', (resp) => {
     let data = '';
 
     // A chunk of data has been recieved.
@@ -11,7 +12,7 @@ export function test(callback) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-      callback(JSON.parse(data).explanation);
+      callback(JSON.parse(data).something);
     });
 
   }).on("error", (err) => {
