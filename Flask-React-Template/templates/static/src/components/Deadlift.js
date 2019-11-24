5c67a0a394eeb297f15d4c71883501f706db9d20
Andreas Papon <andreas.papon@tum.de>
2019-11-24 04:57:47 +0100

    try fix stuff.

--- Flask-React-Template/templates/static/src/components/Deadlift.js
+++ Flask-React-Template/templates/static/src/components/Deadlift.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import {Test} from "./Test";
 
 const Deadlift = ({status, count, setScreen}) => {
   const message = status === 'good' ? <div className="message-prim">Keep it up!</div> : (<React.Fragment>
@@ -10,7 +11,8 @@ const Deadlift = ({status, count, setScreen}) => {
   return (
     <div className={classNames('ex-deadlift', status === 'good' ? 'good' : 'bad')}>
       <div className="message">{message}</div>
-      <div className="count">{count}</div>
+      {/*<div className="count">{count}</div>*/}
+      <Test/>
       <div className="actions">
         <div className="action button finish" onClick={() => {
           setScreen('end');
--- Flask-React-Template/templates/static/src/components/Test.js
+++ Flask-React-Template/templates/static/src/components/Test.js
@@ -1,19 +1,4 @@
 import React from 'react'
-import * as ApiClient from "../ApiClient";
-import Visualization from "./Visualization";
-
-const Test = () => {
-  const [text, setText] = React.useState('');
-  React.useEffect(() => {
-    ApiClient.test(x => setText(x));
-  });
-  return (
-    <div>
-      {text}
-      <Visualization value={text}/>
-    </div>
-  )
-};
 import {IgrRadialGaugeModule} from "igniteui-react-gauges/ES5/igr-radial-gauge-module";
 import {IgrRadialGauge} from "igniteui-react-gauges/ES5/igr-radial-gauge";
 import {IgrRadialGaugeRange} from "igniteui-react-gauges/ES5/igr-radial-gauge-range";
